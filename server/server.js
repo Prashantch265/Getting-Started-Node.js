b667f3766215efe10e02f78eaa7a562cfb79e252
Prashant Chaudhary <Prashantchy265@gmail.com>
2021-05-03 10:06:02 +0545

    Initital commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const app = require('./express');
+const sequelize = require('../database/models');
+require('dotenv').config();
+
+let port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server started on ${port}`));
+
+try{
+    sequelize.sync({alter: true})
+    .then(()=> console.log("DB connected"));
+}catch(err){
+    console.log(err);
+}
